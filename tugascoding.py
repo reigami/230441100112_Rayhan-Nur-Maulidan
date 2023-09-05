311457c438cbb0d78e7f07bbe15920d6d1cba9d0
reigami <143861039+reigami@users.noreply.github.com>
2023-09-05 21:07:21 +0700

    Add files via upload
    Nama : Rayhan Nur Maulidan
    NIM : 230441100112

---
+++ tugascoding.py
@@ -0,0 +1,50 @@
+nama_saya = str("nama saya RAYHAN NUR MAULIDAN ")
+print(nama_saya, type(nama_saya))
+
+NIM = str("NIM= 230441100112")
+print (NIM, type(NIM))
+
+print()
+print()
+
+print("Menghitung_Volume_Balok")
+panjang_balok = int(input("masukkan panjang balok = "))
+print(panjang_balok, type(panjang_balok))
+
+lebar_balok = int(input("masukkan lebar balok = "))
+print(lebar_balok, type(lebar_balok))
+
+tinggi_balok = int(input("masukkan tinggi balok = "))
+print(tinggi_balok, type(tinggi_balok))
+
+volume_balok = str("volume balok adalah = ")
+print("volume_balok adalah = ", panjang_balok*lebar_balok*tinggi_balok, type(volume_balok))
+
+print()
+print()
+
+print("Menghitung_Volume_Tabung")
+phi= 22/7
+print(phi, type(phi))
+
+jari_tabung = int(input("masukkan jari tabung = "))
+print(jari_tabung, type(jari_tabung))
+
+tinggi_tabung = int(input("masukkan tinggi tabung = "))
+print(tinggi_tabung, type(tinggi_tabung))
+
+volume_tabung = str("volume tabung adalah = ")
+print("volume tabung adalah = ", (phi*jari_tabung*jari_tabung*tinggi_tabung), type(volume_tabung))
+
+print()
+print()
+
+print("Menghitung_Volume_Bola")
+phi= 22/7
+print(phi, type(phi))
+
+jari_bola = int(input("masukkan jari bola = "))
+print(jari_bola, type(jari_bola))
+
+volume_bola = str("volume bola adalah = ")
+print("volume bola adalah = ", (4/3*phi*jari_bola*jari_bola*jari_bola), type(volume_bola))
